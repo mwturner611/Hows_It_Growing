a6375a652ae20cf44f0c75d251a2ae065ca5439e
mwturner611 <mwturner611@gmail.com>
2020-05-11 18:59:33 -0500

    db starter files

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,20 @@
-CREATE DATABASE plants; 
+CREATE DATABASE howsitgrowing; 
+
+USE howsitgrowing;
+
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    userName varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE plants(
+    id int NOT NULL AUTO_INCREMENT,
+    plantType varchar(255) NOT NULL,
+    nickName varchar(255) NOT NULL,
+    waterDay1 varchar(20) NOT NULL,
+    waterDay2 varchar,
+    userId int NOT NULL,
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE howsitgrowing;
+
+INSERT INTO users (userName,password) VALUES ('Kevon','password');
+INSERT INTO users (userName,password) VALUES ('Jane','password');
+
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('fern','fernie','Wednesday',1);
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('tree','branch','Wednesday',1);
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('bush','wanda','Wednesday',1);
+
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('fern','phillip','Thursday',2);
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('palm tree','denise','Monday',2);
+INSERT INTO plants (plantType,nickName,waterday1,userId) VALUES ('oak tree','helena','Wednesday',2);
