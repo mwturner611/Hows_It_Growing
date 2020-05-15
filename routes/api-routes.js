cb35651fcba23b95bc479879f27996eb737bae85
P Bridwell <patbridwell@cox.net>
2020-05-15 13:41:13 -0500

    adds button to login form and adjusts spacing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,14 +12,9 @@ var db = require('../models');
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the todos
+  // GET route for getting all of the users plants 
   app.get("/", function(req, res) {
       res.json('route hit')
-    // findAll returns all entries for a table when used with no options
-    // db.Todo.findAll({}).then(function(dbTodo) {
-    //   // We have access to the todos as an argument inside of the callback function
-    //   res.json(dbTodo);
-    // });
   });
 
   // ----------Testing route for creating new user.------------ (works through postman)
