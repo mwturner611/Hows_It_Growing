ba07c1336e1e2eed31950de02502d0760966900c
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-05-19 00:01:29 -0500

    styling and watered button added

--- public/js/plant.js
+++ public/js/plant.js
@@ -3,6 +3,37 @@ $(document).ready(function(){
     // activate tabs on the listed plant card
     $('.tabs').tabs();
 
+    $('#watered').on('click', function(e) {
+        e.preventDefault();
+        
+        var id = $(this).data('id');
+        var newWateredState = {
+            watered: 1
+        };
+
+        $.ajax('/api/plant/' + id, {
+            type: 'PUT',
+            data: newWateredState
+        }).then(
+            function() {
+            //   $('#watercan-icon').removeClass('hide');
+            getWateredState(id)
+            }
+        );
+    });
+
+    // var getWateredState = function(id) {
+
+    //     $.ajax('api/plant/' + id, {
+    //         type: 'GET',
+    //         dataType: 'json'
+    //     }).then(
+    //         function(data) {
+    //             console.log(data);
+    //         }
+    //     );
+    // };
+
 });
 
 
