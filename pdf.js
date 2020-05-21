0a8416fd366d6908816fc0b6f489a75ea6425261
John Slota <jslota17@comcast.net>
2020-05-21 12:40:02 -0500

    Created output source for PDF

--- pdf.js
+++ pdf.js
@@ -4,7 +4,16 @@ const blobStream  = require('blob-stream');
 //Create a document
 const doc = new PDFDocument;
 
-// pipe the document to a blob
+//Pipe PDF to a blob
 const stream = doc.pipe(blobStream());
 
+// get a blob when you're done
 doc.end();
+stream.on('finish', function() {
+  // get a blob you can do whatever you like with
+  const blob = stream.toBlob('application/pdf');
+
+  // or get a blob URL for display in the browser
+  const url = stream.toBlobURL('application/pdf');
+  iframe.src = url;
+});
